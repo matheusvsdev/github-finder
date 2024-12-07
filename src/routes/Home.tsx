93f270108f93c64915275aa649a185fa6e6a637c
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2024-12-07 13:40:18 -0300

    Retrieving user via GitHub API

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { UserProps } from "../types/user";
+import Search from "../components/Search";
+import { useState } from "react";
 
 const Home = () => {
-    return <div>Home</div>
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <Search loadUser={loadUser}/>
+    </div>
+  );
 };
 
 export default Home;
